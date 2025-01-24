b981bd83eef959524cfbf2fa23bdb447ecf9c373
oliviacortiade <o.cortiade@gmail.com>
2025-01-24 20:00:15 +0100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -153,5 +153,26 @@ $(window).load(function(){
 
     portfolioIsotope.isotope({ filter: $(this).data('filter') });
   });
+var magnifPopup = function() {
+  $('.popup-img').not('.no-popup').magnificPopup({
+    type: 'image',
+    removalDelay: 300,
+    mainClass: 'mfp-with-zoom',
+    gallery: {
+      enabled: true
+    },
+    zoom: {
+      enabled: true,
+      duration: 300,
+      easing: 'ease-in-out',
+      opener: function(openerElement) {
+        return openerElement.is('img') ? openerElement : openerElement.find('img');
+      }
+    }
+  });
+};
 
+// Appelle la fonction
+magnifPopup();
+  });
 })
